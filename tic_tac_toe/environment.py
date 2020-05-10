002153d8498809baed3c0f636536e0ea515d50af
cecilia_laborde <cecilialaborde@gmail.com>
2020-05-10 14:06:40 +0200

    Implemented if winner exists

--- tic_tac_toe/environment.py
+++ tic_tac_toe/environment.py
@@ -7,11 +7,13 @@ class Environment:
                       [0, 0, 0]]
         self.x = 1
         self.o = 2
-        self.map = {"0": 0, "x": 1, "o": 2}
+        # self.map = {0: 0, "x": 1, "o": 2}
         self.state = None
 
-    def game_over(self):
-        return "True or False"
+    def game_over(self, force_recalculate=False):
+        if 0 not in self.board or self.winner_exists()[1]:
+            return True
+        return False
 
     def draw_board(self):
         return None
@@ -22,7 +24,29 @@ class Environment:
         hash_ = 0
         for i in range(self.length):
             for j in range(self.length):
-                v = self.map.get(self.board[i, j])
+                v = self.board[i, j]
                 hash_ += (3**k) * v
                 k += 2
         return hash_
+
+    def winner_exists(self):
+        found_winner = False
+        winner = None
+        while not found_winner:
+            for i in range(self.lenght):
+                if len(set(self.board[i])) == 1:
+                    winner = set(self.board[i])
+                    found_winner = True
+                    break
+            for i in range(self.lenght):
+                if len(set(self.board[:, i])) == 1:
+                    winner = set(self.board[:, i])
+                    found_winner = True
+                    break
+            if self.board[0, 0] == self.board[2, 2] == self.board[2, 2]:
+                winner = self.board[0, 0]
+                found_winner = True
+            if self.board[0, 2] == self.board[2, 2] == self.board[2, 0]:
+                winner = self.board[0, 2]
+                found_winner = True
+        return (winner, found_winner)
