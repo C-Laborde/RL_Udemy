35bc1b8708af2f8ead3ebe784bc788bde44cfa70
cecilia_laborde <cecilialaborde@gmail.com>
2020-05-10 13:25:38 +0200

    Implemented get_state

--- tic_tac_toe/environment.py
+++ tic_tac_toe/environment.py
@@ -1,6 +1,13 @@
 class Environment:
     def __init__(self):
         self.game_over = False
+        self.length = 3
+        self.board = [[0, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]]
+        self.x = 1
+        self.o = 2
+        self.map = {"0": 0, "x": 1, "o": 2}
         self.state = None
 
     def game_over(self):
@@ -10,4 +17,12 @@ class Environment:
         return None
 
     def get_state(self):
-        return "state"
+        # base 3 representation of the state
+        k = 0
+        hash_ = 0
+        for i in range(self.length):
+            for j in range(self.length):
+                v = self.map.get(self.board[i, j])
+                hash_ += (3**k) * v
+                k += 2
+        return hash_
