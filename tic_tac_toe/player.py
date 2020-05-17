465df8f3c6ceb11cba774f1780b6f88ea817ad80
cecilia_laborde <cecilialaborde@gmail.com>
2020-05-17 12:01:42 +0200

    Implemented easiest methods

--- tic_tac_toe/player.py
+++ tic_tac_toe/player.py
@@ -1,15 +1,35 @@
 class Player:
-    def __init__(self):
+    def __init__(self, eps=0.1, alpha=0.5):
+        self.eps = eps  # Prob of choosing random action instead of greedy
+        self.alpha = alpha  # Learning rate for the update equation
+        self.verbose = False
+        self.state_hisotry = []
         self.turn = 1
 
     def setV(self, V):
-        return
+        """
+        Initializes the value function
+        """
+        self.V = V
     
     def set_symbol(self, symbol):
-        return
+        """
+        Assigns symbol to agent to use in the board
+        """
+        self.sym = sym
     
+    def set_verbose(self, verbose=False):
+        """
+        Prints out additional info
+        """
+        self.verbose = v
+
     def reset_history(self):
-        return
+        """
+        Once the episode is finished we don't need the history anymore. The
+        history is reseted before starting a new episode 
+        """
+        self.state_hisotry = []
     
     def take_action(self, env):
         return "do something on the env"
